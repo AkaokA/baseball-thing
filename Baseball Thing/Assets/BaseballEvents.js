26de9563deac59d64c8c8c8ae195493827b896f4
Eric Akaoka <eric.akaoka@gmail.com>
2015-12-12 01:51:29 -0500

    I AM LEARNING A LOT.

---
+++ Baseball Thing/Assets/BaseballEvents.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnMouseDown() {
+	GetComponent.<Rigidbody>().AddForce( (transform.forward + Vector3(0,0.5,0) ) * 500f);
+}
