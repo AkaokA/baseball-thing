29e6c1c9dd5864dbaadd11bea7a494a3389f916b
Eric Akaoka <eric.akaoka@gmail.com>
2015-12-12 21:26:38 -0500

    Figuring shit out + organization.

---
+++ Baseball Thing/Assets/Scripts/BaseballEvents.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var hitForce : float = 500f;
+var hitDirection = Vector3(0,0.5,0);
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnMouseDown() {
+	GetComponent.<Rigidbody>().AddForce( hitDirection.normalized * hitForce);
+}
