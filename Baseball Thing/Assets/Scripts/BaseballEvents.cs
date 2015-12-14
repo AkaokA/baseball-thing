e83cc4880fcb0ccfe08b272bcff2ff64dc860b89
Eric Akaoka <eakaoka@kineticcafe.com>
2015-12-14 18:12:25 -0500

    Allow choppers.

--- Baseball Thing/Assets/Scripts/BaseballEvents.cs
+++ Baseball Thing/Assets/Scripts/BaseballEvents.cs
@@ -22,7 +22,6 @@ public class BaseballEvents : MonoBehaviour {
 	public void HitBaseball() {
 		hitDirection = Random.onUnitSphere;
 		hitDirection.x = Mathf.Abs(hitDirection.x);
-		hitDirection.y = Mathf.Abs(hitDirection.y);
 		hitDirection.z = Mathf.Abs(hitDirection.z);
 
 		hitForce = Random.Range (500, 1500);
