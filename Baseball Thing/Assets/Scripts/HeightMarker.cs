c8c28fdb0e3ba5a4f4da99bdc52af4bed48447d9
Eric Akaoka <eakaoka@kineticcafe.com>
2015-12-14 18:31:05 -0500

    Height marker ssssoooort of.

---
+++ Baseball Thing/Assets/Scripts/HeightMarker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightMarker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tempPosition = transform.localPosition;
+		tempPosition.y = -transform.parent.position.y*2 + 0.002f;
+		transform.localPosition = tempPosition;
+
+		Quaternion tempRotation = transform.rotation;
+		tempRotation = Quaternion.Euler(90, 0, 0);
+		transform.rotation = tempRotation;
+	}
+}
