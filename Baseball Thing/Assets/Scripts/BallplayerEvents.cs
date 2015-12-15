989f6107a8b5f1d7f7ed12a64361577797600189
Eric Akaoka <eric.akaoka@gmail.com>
2015-12-15 13:01:44 -0500

    Laggy dragging.

--- Baseball Thing/Assets/Scripts/BallplayerEvents.cs
+++ Baseball Thing/Assets/Scripts/BallplayerEvents.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BallplayerEvents : MonoBehaviour {
 
-	public float moveSpeed = 0.2f;
+	public float dragAmount = 2f;
 	private RaycastHit hit;
 
 	// Use this for initialization
@@ -16,28 +16,6 @@ public class BallplayerEvents : MonoBehaviour {
 		
 	}
 
-	void OnMouseUp () {
-		gameObject.layer = 0;
-		StartCoroutine (MoveBallplayer ());
-	}
-
-	public GameObject dragParticles;
-
-	IEnumerator SpawnDragParticles () {
-		GameObject dragParticleInstance = Instantiate (dragParticles);
-		dragParticleInstance.transform.position = hit.point;
-		yield return new WaitForSeconds(0.5f);
-		Destroy (dragParticleInstance);
-		yield return null;
-	}
-
-	IEnumerator MoveBallplayer () {
-		while (Vector3.Distance (transform.position, hit.point) > 0) {
-			transform.position = Vector3.MoveTowards (transform.position, hit.point, moveSpeed);
-			yield return null;
-		}
-	}
-
 	void OnMouseDrag () {
 		gameObject.layer = 2;
 
@@ -46,8 +24,40 @@ public class BallplayerEvents : MonoBehaviour {
 		// Casts the ray and get the first game object hit
 		Physics.Raycast(ray, out hit);
 
-		StartCoroutine (SpawnDragParticles ());
+		//		StartCoroutine (SpawnDragParticles ());
 
-		transform.position = Vector3.MoveTowards (transform.position, hit.point, moveSpeed);
+		pushBallplayerToPoint ();
 	}
+
+	void OnMouseUp () {
+		gameObject.layer = 0;
+		StartCoroutine (MoveBallplayer ());
+	}
+
+	IEnumerator MoveBallplayer () {
+		while (Vector3.Distance (transform.position, hit.point) > 0) {
+			pushBallplayerToPoint ();
+			yield return null;
+		}
+	}
+		
+	void pushBallplayerToPoint () {
+		Vector3 heading = hit.point - transform.position;
+		GetComponent<Rigidbody> ().AddForce (heading);
+		float distanceToPoint = heading.magnitude;
+		GetComponent<Rigidbody> ().drag = dragAmount / distanceToPoint;
+
+//		transform.rotation = Quaternion.RotateTowards (transform.rotation, hit.point, 1);
+	}
+
+//	public GameObject dragParticles;
+//
+//	IEnumerator SpawnDragParticles () {
+//		GameObject dragParticleInstance = Instantiate (dragParticles);
+//		dragParticleInstance.transform.position = hit.point;
+//		yield return new WaitForSeconds(0.5f);
+//		Destroy (dragParticleInstance);
+//		yield return null;
+//	}
+
 }
